25cdeb0c5841729c3e96cce56025b387855cc1f5
parulian56 <arthaparulian31@gmail.com>
2025-01-29 17:37:07 +0700

    yayaya

--- resources/views/dataa/edit.blade.php
+++ resources/views/dataa/edit.blade.php
@@ -1,12 +1,12 @@
 @extends('main')
 
 @section('content')
-    <h2>Edit Murid</h2>
-    <form action="{{ route('murid.update', $murid->id) }}" method="POST">
+    <h2>Edit Data</h2>
+    <form action="{{ route('data.update', $data->id) }}" method="POST">
         @csrf @method('PUT')
-        <input type="text" name="nama" value="{{ $murid->nama }}">
-        <input type="text" name="kelas" value="{{ $murid->kelas }}">
-        <input type="text" name="jurusan" value="{{ $murid->jurusan }}">
+        <input type="text" name="nama" value="{{ $data->nama }}">
+        <input type="text" name="kelas" value="{{ $data->kelas }}">
+        <input type="text" name="jurusan" value="{{ $data->jurusan }}">
         <button type="submit">Update</button>
     </form>
 @endsection
