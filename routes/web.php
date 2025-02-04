2f2c625ea61bf15224a83811b244e463e4a9ad22
FATTFAWW <164737174+FATTFAWW@users.noreply.github.com>
2025-02-04 13:26:11 +0800

    sss

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 use App\Http\Controllers\TransaksiController;
-use App\Http\Controllers\AppController;
 use App\Http\Controllers\DataController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
@@ -9,9 +8,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('data', DataController::class);
-Route::resource('App', AppController::class);
 Route::resource('report', ReportController::class);
 
 
 
 Route::resource('transaksi', TransaksiController::class);
+use App\Http\Controllers\DashboardController;
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
