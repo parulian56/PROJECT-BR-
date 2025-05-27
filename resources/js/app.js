fb91fd848009f7ceb4e1af45f3713aafec8ce7e7
FATTFAWW <164737174+FATTFAWW@users.noreply.github.com>
2025-05-27 09:19:41 -0700

    update tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,12 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect';
+import focus from '@alpinejs/focus';
+import Chart from 'chart.js/auto';
+
+Alpine.plugin(intersect);
+Alpine.plugin(focus);
 
 window.Alpine = Alpine;
-
 Alpine.start();
-
-import Alpine from 'alpinejs'
-import intersect from '@alpinejs/intersect'
-import focus from '@alpinejs/focus'
-import Chart from 'chart.js/auto'
-
-window.Alpine = Alpine
-Alpine.plugin(intersect)
-Alpine.plugin(focus)
-Alpine.start()
